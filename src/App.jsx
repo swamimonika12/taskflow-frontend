378e99dc1b9a646226c6aa31636d168b2746f4ce
swamimonika12 <swamimonika4@gmail.com>
2026-04-07 22:11:07 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Board from './pages/Board'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-TEST" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/board/:id" element={<Board />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
